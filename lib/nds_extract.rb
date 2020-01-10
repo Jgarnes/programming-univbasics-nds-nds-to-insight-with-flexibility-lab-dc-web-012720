4295e1dfe94e171f63700665cfc62b8e914c49c0
Jeremy Garnes <j06garnes@gmail.com>
2020-01-10 06:51:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -69,7 +69,7 @@ def gross_per_studio(collection)
   if !new_hash[studio_name]
       new_hash[studio_name] = 1
 else
-  new_hash[studio_name] += 1
+  new_hash[studio_gross] += 1
     end
     i += 1 
   end 
