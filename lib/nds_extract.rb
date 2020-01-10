3fc125b7be81281533bfab00e197c016fecd4a3e
Jeremy Garnes <j06garnes@gmail.com>
2020-01-10 06:49:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -67,7 +67,7 @@ def gross_per_studio(collection)
     studio_name = collection[i][:studio]
     studio_gross = collection[i][:worldwide_gross]
   if !new_hash[studio_name]
-      new_hash[studio_name] = 0
+      new_hash[studio_name] = 1
 else
   new_hash[studio_name] += 0 
     end
